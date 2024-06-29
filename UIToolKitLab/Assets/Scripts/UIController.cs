6e4274242065d2da67a8cf1cc505ee0c89a1efbb
ZerrY <suhan0304@gmail.com>
2024-06-29 15:29:31 +0900

    Dotween

--- UIToolKitLab/Assets/Scripts/UIController.cs
+++ UIToolKitLab/Assets/Scripts/UIController.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using DG.Tweening;
+using TMPro.EditorUtilities;
+using DG.Tweening.Core;
 
 public class UIController : MonoBehaviour
 {
@@ -17,6 +20,8 @@ public class UIController : MonoBehaviour
 
     private VisualElement _boy;
     private VisualElement _girl;
+
+    private Label _message;
 
     private void Start() {
         // Find Root-Element
@@ -33,6 +38,8 @@ public class UIController : MonoBehaviour
 
         _boy = root.Q<VisualElement>("Image_Boy");
         _girl = root.Q<VisualElement>("Image_Girl");
+
+        _message = root.Q<Label>("Message");
 
         // Hide bottom-sheet
         _bottomContainer.style.display = DisplayStyle.None;
@@ -69,11 +76,15 @@ public class UIController : MonoBehaviour
 
     private void AnimateGirl() {
         _girl.ToggleInClassList("image--girl--up");
-        _girl.RegisterCallback<TransitionEndEvent>(AnimateBackGirl);
+        _girl.RegisterCallback<TransitionEndEvent>(
+            evt => _girl.ToggleInClassList("image--girl--up")
+        );
+
+        // Typing Animation
+        _message.text = string.Empty;
+        string m = "\"Sed in rebus apertissimis nimium longi sumus.\"";
+        DOTween.To(() => _message.text, x => _message.text = x, m, 3f).SetEase(Ease.Linear);
     }
 
-    private void AnimateBackGirl(TransitionEndEvent evt) {
-        _girl.ToggleInClassList("image--girl--up");
-    }
 }
 
