23c207e0da02c0cd40c049d078cc28989eb44575
ZerrY <suhan0304@gmail.com>
2024-06-29 14:18:20 +0900

    Update

--- UIToolKitLab/Assets/Scripts/UIController.cs
+++ UIToolKitLab/Assets/Scripts/UIController.cs
@@ -12,14 +12,21 @@ public class UIController : MonoBehaviour
     private Button _openButton;
     private Button _closeButton;
 
+    private VisualElement _bottomSheet;
+    private VisualElement _scrim;
+
     private void Start() {
         // Find Root-Element
         var root = GetComponent<UIDocument>().rootVisualElement; 
 
         // UQuery
         _bottomContainer = root.Q<VisualElement>("Container_Bottom");
+
         _openButton = root.Q<Button>("Button_Open");
         _closeButton = root.Q<Button>("Button_Close");
+
+        _bottomSheet = root.Q<VisualElement>("BottomSheet");
+        _scrim = root.Q<VisualElement>("Scrim");
 
         // Hide bottom-sheet
         _bottomContainer.style.display = DisplayStyle.None;
@@ -33,11 +40,17 @@ public class UIController : MonoBehaviour
     private void OnOpenButtonClicked(ClickEvent evt) {
         // Open Bottom-Sheet
         _bottomContainer.style.display = DisplayStyle.Flex;
+
+        _bottomSheet.AddToClassList("buttomsheet--up");
+        _scrim.AddToClassList("Scrim--fadein");
     }
 
     private void OnCloseButtonClicked(ClickEvent evt) {
         // Close Bottom-Sheet
         _bottomContainer.style.display = DisplayStyle.None;
+
+        _bottomSheet.RemoveFromClassList("buttomsheet--up");
+        _scrim.RemoveFromClassList("Scrim--fadein");
     }
 }
 
