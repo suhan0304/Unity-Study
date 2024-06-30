3ab17d3e9c47fc191859a7eb63beff405e6824a9
ZerrY <suhan0304@gmail.com>
2024-06-30 18:42:13 +0900

    Update UIScreenController.cs

--- UILAB/Assets/Scripts/UI/Controllers/UIScreenController.cs
+++ UILAB/Assets/Scripts/UI/Controllers/UIScreenController.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class UIScreenController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("UI Elements")]
+    [Tooltip("UI Document")]
+    [SerializeField] UIDocument m_Document;
+    [SerializeField] Button m_CoinButton;
+    [SerializeField] Button m_ResetButton;
+
+    VisualElement root;
+
+    private const string coinButtonID = "Button--Coin";
+    private const string coinResetID = "Button--Reset";
+
+    void OnEnable() {
+        root = m_Document.rootVisualElement;
+        m_CoinButton = root.Q<Button>(coinButtonID);
+        m_ResetButton = root.Q<Button>(coinResetID);
+
+        m_CoinButton.RegisterCallback<ClickEvent>(OnClickCoinButton);
+        m_ResetButton.RegisterCallback<ClickEvent>(OnClickResetButton);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnDisable() {
+        m_CoinButton.UnregisterCallback<ClickEvent>(OnClickCoinButton);
+        m_ResetButton.UnregisterCallback<ClickEvent>(OnClickResetButton);
+    }
+
+    void OnClickCoinButton(ClickEvent evt) {
+
+        Vector2 clickPos = new Vector2(evt.position.x, evt.position.y);
+        Vector2 screenPos = clickPos.GetScreenCoordinate(root);
+
+        CoinEvents.CoinButtonClick?.Invoke(screenPos);
+    }
+
+    void OnClickResetButton(ClickEvent evt) {
+        return;
     }
 }
