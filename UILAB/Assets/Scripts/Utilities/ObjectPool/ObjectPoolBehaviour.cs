c1fddc5ff97b8e4b1e712d6d2d1373567490810c
ZerrY <suhan0304@gmail.com>
2024-06-30 16:34:03 +0900

    Update

---
+++ UILAB/Assets/Scripts/Utilities/ObjectPool/ObjectPoolBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolBehaviour : MonoBehaviour
+{
+    public ObjectPoolData data;
+
+    private List<GameObject> pooledObjects;
+
+    void Awake() {
+        CreatePool();
+    }
+
+    public void CreatePool
+
+}
---
+++ UILAB/Assets/Scripts/Utilities/ObjectPool/ObjectPoolData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data_ObjectPool_", menuName = "Utilities/ObjectPool", order = 1)]
+public class ObjectPoolData : ScriptableObject {
+    [Header("Settings")]
+    public GameObject objectToPool;
+    public int amountToPool;
+    public bool shouldExpand;
+}
