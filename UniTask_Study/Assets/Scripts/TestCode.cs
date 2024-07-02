44a73c4629e6506ba6181faf6493037a377de8e4
ZerrY <suhan0304@gmail.com>
2024-07-02 20:40:14 +0900

    Update TestCode.cs

--- UniTask_Study/Assets/Scripts/TestCode.cs
+++ UniTask_Study/Assets/Scripts/TestCode.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
@@ -13,6 +14,8 @@ public class TestCode : MonoBehaviour
     private const string AppleImagePath = "https://cdn.tridge.com/attachment-file/8d/e0/d9/8de0d913f1bd9438ce1ab69086961e2c884877bd/apple.jpeg";
     public RawImage profileImage;
 
+    
+    //Coroutine
     private IEnumerator WaitGetWebTexture(UnityAction<Texture2D> action) {
         // 코루틴은 IEnumrator 형이기 때문에 가져온 Texture를 반환 할 수는 없다.
         // => Action을 추가로 달아주어야 한다.
@@ -32,9 +35,32 @@ public class TestCode : MonoBehaviour
         }
     }
 
-    private void Start() {
-        StartCoroutine(WaitGetWebTexture(texture => {
-            profileImage.texture = texture;
-        }));
+    // UniTask
+    private async UniTask<Texture2D> WaitGetWebTextureAsync() {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(AppleImagePath);
+        await request.SendWebRequest();
+
+        // Connection or Protocol Error가 발생한 경우
+        if (request.result is UnityWebRequest.Result.ConnectionError or UnityWebRequest.Result.ProtocolError) {
+            // Error 처리
+            Debug.LogError(request.error);
+        }
+        else {
+            // Reqeust 성공
+            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            return texture;
+        }
+        return null;
     }
+
+    private async UniTaskVoid GetImageAsync() {
+        Texture2D texture = await WaitGetWebTextureAsync();
+        profileImage.texture = texture;
+    }
+
+    private void Start() {
+        GetImageAsync().Forget();
+    }
+    
+
 }
