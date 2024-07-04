109bdd9fdbea65d7149e261d67f08261ff9c6ab6
ZerrY <suhan0304@gmail.com>
2024-07-04 13:51:37 +0900

    Update

--- UniTask_Study_2/Assets/Scripts/AnimateScene.cs
+++ UniTask_Study_2/Assets/Scripts/AnimateScene.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using UnityEngine;
 
 public class AnimateScene : MonoBehaviour
 {
     [SerializeField] private Transform[] outerObjectsTransform;
+
+    async void Start() {
+        await AnimateOuterObject(outerObjectsTransform, this.GetCancellationTokenOnDestroy());
+    }
+
+    private async UniTask AnimateOuterObject(Transform[] objectsToAnimate, CancellationToken cancelltationToken) 
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(0.2f), cancellationToken: cancelltationToken);
+        for (int i = 0; i < objectsToAnimate.Length; i++) {
+            objectsToAnimate[i].gameObject.SetActive(true);
+            objectsToAnimate[i].DOScale(Vector3.zero, 0.5f).From().SetEase(Ease.InOutBack).WithCancellation(cancelltationToken);
+            await UniTask.Delay(TimeSpan.FromSeconds(0.1f), cancellationToken: cancelltationToken);
+        }
+    }
 }
