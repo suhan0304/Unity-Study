9d24ec9d1f1e4948112347d7da62fab1f98d651d
ZerrY <suhan0304@gmail.com>
2024-07-09 14:13:05 +0900

    Update

---
+++ UIToolKit_CustomMenu/Assets/Scripts/Control_Button_Manager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Control_Button_Manager : MonoBehaviour
+{
+    [SerializeField] private UIDocument UI_Doc;
+    [SerializeField] private List<Control_Button> controlButtons;
+
+    [SerializeField] private Control_Button selectedButton;
+
+    [Button("Init Control Button List")]
+    void Init() {
+        var root = UI_Doc.rootVisualElement;
+
+        // Load Control Button in UI_Doc
+        controlButtons = new List<Control_Button>();
+        var buttons = root.Query<Control_Button>().ToList();
+        foreach (var button in buttons) {
+            controlButtons.Add(button);
+        }
+        Debug.Log($"[Control Button Manager] {controlButtons.Count} Buttons Initialized");
+    }
+}
