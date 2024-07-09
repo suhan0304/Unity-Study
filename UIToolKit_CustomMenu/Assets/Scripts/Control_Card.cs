10d8deaf93843273f7f87387b545b5fb7e221d4c
ZerrY <suhan0304@gmail.com>
2024-07-09 15:14:10 +0900

    Update

--- UIToolKit_CustomMenu/Assets/Scripts/Control_Card.cs
+++ UIToolKit_CustomMenu/Assets/Scripts/Control_Card.cs
@@ -44,9 +44,38 @@ public class Control_Card : VisualElement
     public event Action<Control_Card> OnHover;
     public event Action<Control_Card, int> OnSelect;
 
-    public Control_Card() {
+    public Control_Card() 
+    {
         // 카드 뒷배경
-        _card_BackGround = new VisualElement();
+        _card_BackGround = new VisualElement { name = "CardBackGround" };
         Add(_card_BackGround);
+
+        // 카드 이미지
+        _card_Image = new VisualElement { name = "CardImage" };
+        _card_BackGround.Add(_card_Image);
+
+        // 카드 레이블
+        _card_Label = new Label { name = "CardLabel" };
+        _card_BackGround.Add(_card_Label);
+
+        // 카드 버튼
+        _card_Button = new VisualElement { name = "CardButton" };
+        Add(_card_Button);
+
+        // 버튼 아이콘
+        _button_icon = new VisualElement { name = "ButtonIcon" };
+        _card_Button.Add(_button_icon);
+
+        // 버튼 레이블
+        _button_label = new Label { name = "ButtonLabel" };
+        _card_Button.Add(_button_label);
+
+        // Preventing Clicks
+        _card_BackGround.pickingMode = PickingMode.Ignore;
+        _card_Image.pickingMode = PickingMode.Ignore;
+        _card_Label.pickingMode = PickingMode.Ignore;
+        _card_Button.pickingMode = PickingMode.Ignore;
+        _button_icon.pickingMode = PickingMode.Ignore;
+        _button_label.pickingMode = PickingMode.Ignore;
     }
 }
