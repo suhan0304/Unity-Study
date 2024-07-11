0b5db174572a49114c86f0e8725276662cc784a5
ZerrY <suhan0304@gmail.com>
2024-07-11 14:23:22 +0900

    Update TabData.cs

--- UIToolKit_CustomMenu/Assets/Scripts/UI/TabData.cs
+++ UIToolKit_CustomMenu/Assets/Scripts/UI/TabData.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Tab_Data", menuName = "Tab_Data", order = 0)]
 public class TabData : ScriptableObject
 {
-    [SerializeField] 
-    public List<CardData> tabData = new List<CardData>();
+    [SerializeField] public List<CardData> tabData = new List<CardData>();
 }
