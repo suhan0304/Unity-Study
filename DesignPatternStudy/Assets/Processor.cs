b7068acac8cf2314e7fa0ed453b66fb596d3769f
ZerrY <suhan0304@gmail.com>
2024-09-06 12:30:13 +0900

    MinusPositionProcessor Update

--- DesignPatternStudy/Assets/Processor.cs
+++ DesignPatternStudy/Assets/Processor.cs
@@ -17,10 +17,26 @@ class PositionProcessor {
     }
 }
 
+class MinusPositionProcessor {
+    public Vector3 process(Transform parent) {
+        Vector3 result = Vector3.zero;
+
+        try {
+            foreach(Transform child in parent) {
+                result -= child.position;
+            }
+        } catch (Exception e) {
+            Debug.LogError(e.Message);
+        }
+    
+        return result;
+    }
+}
+
 public class Processor : MonoBehaviour
 {
     private void Start() {
-        PositionProcessor positionProcessor = new PositionProcessor();
+        MinusPositionProcessor positionProcessor = new MinusPositionProcessor();
 
         Vector3 totalPosition = positionProcessor.process(this.transform);
         
