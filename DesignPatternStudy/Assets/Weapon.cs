29c87f461b77b75fad3c8fd8e1397cbddf6245b2
ZerrY <suhan0304@gmail.com>
2024-09-12 12:51:03 +0900

    feat: Update Weapon with Strategy Pattern

---
+++ DesignPatternStudy/Assets/Weapon.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public interface Weapon {
+    void offensive();
+}
+
+public class Sword : Weapon {
+    public void offensive() {
+        Debug.Log("칼을 휘두르다.");
+    }
+}
+
+public class Shield : Weapon {
+    public void offensive() {
+        Debug.Log("방패로 밀치다.");
+    }
+}
+
+public class Crossbow : Weapon {
+    public void offensive() {
+        Debug.Log("석궁을 발사하다.");
+    }
+}
