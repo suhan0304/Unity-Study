eed42a9507db838cfb785cb4e51ce5e332a3e5f6
ZerrY <suhan0304@gmail.com>
2024-09-27 15:29:03 +0900

    Fix: Base Data Update to data (not data1)

--- DesignPatternStudy/Assets/DataManager.cs
+++ DesignPatternStudy/Assets/DataManager.cs
@@ -117,14 +117,14 @@ public class DataManager : MonoBehaviour
         Debug.Log("------------");
         
         // 동서시성이 적용된 로직 안의 코드의 시간 측정을 하고 싶을 때
-        IData data2 = new SynchronizedDecorator(new timerMeasureDecorator(data1));
+        IData data2 = new SynchronizedDecorator(new timerMeasureDecorator(data));
         data2.setData(24680);
         Debug.Log(data2.getData());
         
         Debug.Log("------------");
         
         // 동시성이 적용된 코드를 시간 측정을 하고 싶을 때
-        IData data3 = new timerMeasureDecorator(new SynchronizedDecorator(data1));
+        IData data3 = new timerMeasureDecorator(new SynchronizedDecorator(data));
         data3.setData(99999);
         Debug.Log(data3.getData());
     }
