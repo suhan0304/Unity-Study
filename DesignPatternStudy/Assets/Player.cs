d9824272862848268acb2646bb38805ec1325f99
ZerrY <suhan0304@gmail.com>
2024-09-27 14:48:29 +0900

    Test: Wrapping order effect on results

--- DesignPatternStudy/Assets/Player.cs
+++ DesignPatternStudy/Assets/Player.cs
@@ -3,21 +3,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     void Start() {
-        // 1. 유탄 발사기가 달린 총
-        Weapon generade_rifle = new Generade(new BaseWeapon());
-        generade_rifle.aim_and_fire();
-        
-        Debug.Log("-------------");
-        
-        // 2. 개머리판을 장착하고 스코프를 달은 총
-        Weapon buttstock_scoped_rifle = new Buttstock(new Generade(new BaseWeapon()));
+        // 1. 개머리판을 장착하고 스코프를 달은 총
+        Weapon buttstock_scoped_rifle = new Buttstock(new Scoped(new BaseWeapon()));
         buttstock_scoped_rifle.aim_and_fire();
         
         Debug.Log("-------------");
-        
-        // 3. 유탄 발사기 + 개머리판 + 스코프가 달린 총
-        Weapon buttstock_scoped_generade_rifle = new Buttstock(new Scoped(new Generade(new BaseWeapon())));
-        buttstock_scoped_generade_rifle.aim_and_fire();
 
+        // 2. 개머리판을 장착하고 스코프를 달은 총
+        Weapon scoped_buttstock_rifle = new Scoped(new Buttstock(new BaseWeapon()));
+        scoped_buttstock_rifle.aim_and_fire();
     }
 }
