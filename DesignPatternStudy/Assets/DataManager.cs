790f47f9814792a606d68783a978d5301f054389
ZerrY <suhan0304@gmail.com>
2024-09-27 15:10:38 +0900

    Refactor: Data Manager Update to Decorator Pattern

--- DesignPatternStudy/Assets/DataManager.cs
+++ DesignPatternStudy/Assets/DataManager.cs
@@ -2,31 +2,68 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-class MyData
+// 원본 객체와 장식된 객체 모두를 묶는 인터페이스
+interface IData
+{
+    void setData(int data);
+    int getData();
+}
+
+// 장식될 원본 객체
+class MyData : IData
 {
     private int data;
 
-    public virtual void setData(int data)
+    public void setData(int data)
     {
         this.data = data;
     }
 
-    public virtual int getData()
+    public int getData()
     {
-        return this.data;
+        return data;
     }
 }
 
-class SynchronizedData : MyData
+// 장식자 추상 클래스
+abstract class MyDataDecorator : IData
 {
-    private readonly object syncLock = new object();
+    private IData mydataObj; // 최상위 인터페이스 타입으로 장식할 원본 객체 선언
+
+    protected MyDataDecorator(IData mydataObj) 
+    {
+        this.mydataObj = mydataObj;
+    }
+
+    public virtual void setData(int data)
+    {
+        this.mydataObj.setData(data); 
+    }
+
+    public virtual int getData()
+    {
+        return mydataObj.getData(); 
+    }
+}
+
+// 장식자 클래스
+class SynchronizedDecorator : MyDataDecorator
+{
+    private readonly object syncLock = new object(); // lock에 사용할 객체
+
+    public SynchronizedDecorator(IData mydataObj) : base(mydataObj)
+    {
+    }
 
     public override void setData(int data)
     {
         lock (syncLock)
         {
-            base.setData(data);
+            Debug.Log("동기화된 data 처리를 시작합니다.");
+            base.setData(data); // 부모 클래스의 setData 호출
+            Debug.Log("동기화된 data 처리를 완료했습니다.");
         }
     }
 
@@ -34,18 +71,36 @@ class SynchronizedData : MyData
     {
         lock (syncLock)
         {
-            return base.getData();
+            Debug.Log("동기화된 data 처리를 시작합니다.");
+            int result = base.getData(); // 부모 클래스의 getData 호출
+            Debug.Log("동기화된 data 처리를 완료했습니다.");
+            return result;
         }
     }
 }
 
+// 나중에 기능 추가가 되도 수정없이 유연하게 클래스만 정의하면 추가 가능
+class AnotherSkillDecorator : MyDataDecorator
+{
+    private IData mydataObj;
+
+    public AnotherSkillDecorator(IData mydataObj) : base(mydataObj)
+    {
+    }
+    
+    // 추가 기능 --- 
+}
 
 public class DataManager : MonoBehaviour
 {
     public void Start()
     {
-        SynchronizedData data = new SynchronizedData();
-        data.setData(1234);
-        Debug.Log(data.getData());
+        // 동시성이 필요없을 때
+        IData data = new MyData();
+        
+        // 동시성이 필요할 때
+        IData dataSync = new SynchronizedDecorator(data);
+        dataSync.setData(13579);
+        Debug.Log(dataSync.getData());
     }
 }
