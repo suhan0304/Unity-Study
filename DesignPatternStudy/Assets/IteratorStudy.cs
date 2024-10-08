f8ff7853ff0a32807c276c856a0c3606e4dbc07e
ZerrY <suhan0304@gmail.com>
2024-10-08 13:46:53 +0900

    Rename: Iterator Script to IteratorStudy

--- DesignPatternStudy/Assets/IteratorStudy.cs
+++ DesignPatternStudy/Assets/IteratorStudy.cs
@@ -70,28 +70,32 @@ class AlphabeticalOrderIterator : Iterator
 }
 
 class WordsCollection : IteratorAggregate
+{
+    List<string> _collection = new List<string>();
+    
+    bool _direction = false;
+    
+    public void ReverseDirection()
     {
-        List<string> _collection = new List<string>();
-        
-        bool _direction = false;
-        
-        public void ReverseDirection()
-        {
-            _direction = !_direction;
-        }
-        
-        public List<string> getItems()
-        {
-            return _collection;
-        }
-        
-        public void AddItem(string item)
-        {
-            this._collection.Add(item);
-        }
-        
-        public override IEnumerator GetEnumerator()
-        {
-            return new AlphabeticalOrderIterator(this, _direction);
-        }
+        _direction = !_direction;
     }
+    
+    public List<string> getItems()
+    {
+        return _collection;
+    }
+    
+    public void AddItem(string item)
+    {
+        this._collection.Add(item);
+    }
+    
+    public override IEnumerator GetEnumerator()
+    {
+        return new AlphabeticalOrderIterator(this, _direction);
+    }
+}
+
+class IteratorStudy : MonoBehaviour {
+    
+}
