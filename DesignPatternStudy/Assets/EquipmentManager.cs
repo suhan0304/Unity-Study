db20b210496f8e4b69a784631ccbf90b45641ae3
ZerrY <suhan0304@gmail.com>
2024-10-10 14:50:10 +0900

    feat: Add EquipmentData Class

--- DesignPatternStudy/Assets/EquipmentManager.cs
+++ DesignPatternStudy/Assets/EquipmentManager.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EquipmentManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+class EquipmentData {
+    private string name;
+    private int physicalDamage;
+    private int magicalDamage;
+    private float attackSpeed;
+
+    public int getPhysicalDamage()
     {
-        
+        return this.physicalDamage;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int getMagicalDamage()
     {
-        
+        return this.magicalDamage;
     }
-}
+
+    public float getAttackSpeed()
+    {
+        return this.attackSpeed;
+    }
+
+
+    public EquipmentData(string name, int physicalDamage, int magicalDamage, float attackSpeed) {
+        this.name = name;
+        this.physicalDamage = physicalDamage;
+        this.magicalDamage = magicalDamage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    
+ }
