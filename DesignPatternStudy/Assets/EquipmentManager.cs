ff20384f21a4eb616991f1c32032d97919c4e911
ZerrY <suhan0304@gmail.com>
2024-10-10 15:05:32 +0900

    feat: Update EquipmentManager

--- DesignPatternStudy/Assets/EquipmentManager.cs
+++ DesignPatternStudy/Assets/EquipmentManager.cs
@@ -79,6 +79,7 @@ public class Cache
     }
 }
 
+// EquipmentData를 출력
 public class Message
 {
     private EquipmentData equipment;
@@ -106,5 +107,50 @@ public class Message
     public string MakeAttackSpeed()
     {
         return "Attack Speed : " + equipment.getAttackSpeed();
+    }
+}
+
+public class EquipmentManager : MonoBehaviour
+{
+    void Start()
+    {
+        // 1. 데이터베이스 생성 & 등록
+        DBMS dbms = new DBMS();
+        dbms.Put("Sword", new EquipmentData("Sword", 50, 20, 1.5f));
+        dbms.Put("Staff", new EquipmentData("Staff", 10, 60, 1.2f));
+        dbms.Put("Dagger", new EquipmentData("Dagger", 30, 10, 2.0f));
+
+        // 2. 캐시 생성
+        Cache cache = new Cache();
+
+        // 3. 트랜잭션에 앞서 먼저 캐시에 데이터가 있는지 조회
+        string name = "Sword";
+        EquipmentData equipment = cache.Get(name);
+
+        // 4. 만약 캐시에 없다면
+        if (equipment == null)
+        {
+            equipment = dbms.Query(name); // DB에 해당 데이터를 조회해서 equipment에 저장하고
+            if (equipment != null)
+            {
+                cache.Put(equipment); // 캐시에 저장
+            }
+        }
+
+        // 5. dbms.Query(name)에서 조회된 값이 있으면
+        if (equipment != null)
+        {
+            Message message = new Message(equipment);
+
+            Debug.Log(message.MakeName());
+            Debug.Log(message.MakePhysicalDamage());
+            Debug.Log(message.MakeMagicalDamage());
+            Debug.Log(message.MakeAttackSpeed());
+        }
+        // 6. 조회된 값이 없으면
+        else
+        {
+            Debug.Log($"{name} 가 데이터베이스에 존재하지 않습니다.");
+        }
     }
 }
