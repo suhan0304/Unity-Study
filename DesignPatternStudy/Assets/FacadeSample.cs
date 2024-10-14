098289e7bf3414c1568dfba07e0a9248f458c53b
ZerrY <suhan0304@gmail.com>
2024-10-14 16:51:29 +0900

    Test: Update Start Method in FacadeSample Class

--- DesignPatternStudy/Assets/FacadeSample.cs
+++ DesignPatternStudy/Assets/FacadeSample.cs
@@ -48,3 +48,13 @@ class FacadeClient {
     }
 
 }
+
+public class FacadeSample : MonoBehaviour {
+    private void Start() {
+        Subsystem1 subsystem1 = new Subsystem1();
+        Subsystem2 subsystem2 = new Subsystem2();
+
+        Facade facade = new Facade(subsystem1, subsystem2);
+        FacadeClient.ClientCode(facade);
+    }
+}
