790ee950c42c01e432f8b386dc69257d1fa3567a
ZerrY <suhan0304@gmail.com>
2024-10-14 16:47:09 +0900

    Test: Update Subsystem2 Class

--- DesignPatternStudy/Assets/Facade.cs
+++ DesignPatternStudy/Assets/Facade.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Subsystem1 {
     public string operation1() {
-        return "Subsystem1 : Ready!";
+        return "Subsystem1 : Ready!\n";
     }
 
-    public string operatioN() {
-        return "Subsystem1 : Go!"
+    public string operationN() {
+        return "Subsystem1 : Go!\n";
+    }
+}
+
+public class Subsystem2 {
+    public string operation1() {
+        return "Subsystem2 : Ready!\n";
+    }
+
+    public string operationZ() {
+        return "Subsystem1 : Fire!\n";
     }
 }
 
 public class Facade {
-    protected Subsystem1 _subsystem1;
-    protected Subsystem2 _subsystem2;
 }
