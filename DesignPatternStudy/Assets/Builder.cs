0501cbb38a453407627e2360615fd05243d10f23
ZerrY <suhan0304@gmail.com>
2024-10-16 13:30:17 +0900

    Test: Gradual constuctor Pattern

--- DesignPatternStudy/Assets/Builder.cs
+++ DesignPatternStudy/Assets/Builder.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+class Hamburger {
+    // 필수 매개변수
+    private int bun;
+    private int patty;
 
-public class Builder : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // 선택 매개변수
+    private int cheese;
+    private int lettuce;
+    private int tomato;
+    private int bacon;
+
+    public Hamburger(int bun, int patty, int cheese, int lettuce, int tomato, int bacon) {
+        this.bun = bun;
+        this.patty = patty;
+        this.cheese = cheese;
+        this.lettuce = lettuce;
+        this.tomato = tomato;
+        this.bacon = bacon;
     }
 
-    // Update is called once per frame
-    void Update()
+    public Hamburger(int bun, int patty, int cheese, int lettuce, int tomato) {
+        this.bun = bun;
+        this.patty = patty;
+        this.cheese = cheese;
+        this.lettuce = lettuce;
+        this.tomato = tomato;
+    }
+    
+
+    public Hamburger(int bun, int patty, int cheese, int lettuce) {
+        this.bun = bun;
+        this.patty = patty;
+        this.cheese = cheese;
+        this.lettuce = lettuce;
+    }
+
+    public Hamburger(int bun, int patty, int cheese) {
+        this.bun = bun;
+        this.patty = patty;
+        this.cheese = cheese;
+    }
+}
+public class Builder : MonoBehaviour
+{
+    void Start()
     {
-        
+        // 모든 재료가 있는 햄버거
+        Hamburger hamburger1 = new Hamburger(2, 1, 2, 4, 6, 8);
+
+        // 빵과 패티 치즈만 있는 햄버거
+        Hamburger hamburger2 = new Hamburger(2, 1, 1);
+
+        // 빵과 패티 베이컨만 있는 햄버거
+        Hamburger hamburger3 = new Hamburger(2, 0, 0, 0, 0, 6);
     }
 }
